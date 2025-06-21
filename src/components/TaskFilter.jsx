4a1b07aa82c61b236025655616a5044aa97fc819
Frances Reagan James <adtgwen@gmail.com>
2025-06-21 02:07:48 -0400

    finished taskfilter changes

--- src/components/TaskFilter.jsx
+++ src/components/TaskFilter.jsx
@@ -1,32 +1,36 @@
-select
+
 function TaskFilter ({onFilter}) {
-  const [selectOption, setSelectOption] = useState("all");
+  // const [selectOption, setSelectOption] = useState("all");
   return (
 <div className="task-filter">
   <h3>Filter tasks</h3>
   <div className="filter-controls">
+ <div>
     <label htmlFor="status-filter"> Status:</label>
 <select 
   id="status-filter"
-  onChange={(event) => onFilter("status", event.target.value)}
-  >
+  onChange={(event) => onFilter("status", event.target.value)}>
   <option value="all">All Statuses</option>
   <option value="pending">Pending</option>
-  <option value=in-progree">In Progress</select>
+  <option value="in-progress">In Progress</option>
   <option value="completed">Completed</option>
-<  </select>
+</select>
 
   </div>
     
-
-    {/* <select  onChange={event => onFilter(event.target.value)}>
-      <option value="all">All Properties</option>
+<div> 
+  <label htmlFor="priority-filter">Priority:</label>
+    <select  
+      id="priority-filter"
+      onChange={(event) => onFilter("priority",event.target.value)}>
+      <option value="all">All Priorities</option>
       <option value="high">High</option>
       <option value="medium">Medium</option>
       <option value="low">Low</option>
-    </select> */}
+    </select>
 </div>
-
-  )
+</div>
+ </div>
+  );
 }
 export default TaskFilter;
