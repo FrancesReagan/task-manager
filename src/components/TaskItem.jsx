393cc691e56dd2e9a61c9c0032e2bb0783bd76c7
Frances Reagan James <adtgwen@gmail.com>
2025-06-21 01:17:44 -0400

    fixed taskitem javascript and jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -9,17 +9,24 @@ function TaskItem({task, onStatusChange, onDelete}){
             <p><strong>Priority:</strong>{task.priority}</p>
             <p><strong>Due Date:</strong>{task.dueDate}</p>
             <p><strong>Status:</strong>{task.status}</p>
-            
-            
-      <select onChange={event => onStatusChange(task.id, event.target.value)}>     
-      <option value="completed">Completed</option>
-      <option value="pending">Pending</option>
-      <option value="in-progress">In Progress</option>
-    </select>
+        <div className="task-actions">
+            <select
+                value={task.status}
+                onChange={(event) => onStatusChange(task.id, event.target.value)}
+                className="status-select">
+                
+                <option value="pending">Pending</option>
+                <option value="in-progress">In Progress</option>
+                <option value="completed">Completed</option>
+            </select>
 
-    <button onClick={() => onDelete (task.id)}> X </button> 
-    </div>
-          
-    )
+            <button onClick={() => onDelete (task.id)}
+              className="delete-button">
+                Delete
+             </button> 
+
+        </div>
+    </div> 
+    );
 }
 export default TaskItem;
